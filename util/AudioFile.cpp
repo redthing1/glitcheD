9cba0d6c05fc10fa507556f9c0f560dfa4858837
xdrie <xdrie@alt.icu>
2019-10-11 16:04:51 -0700

    minor cleanup

--- util/AudioFile.cpp
+++ util/AudioFile.cpp
@@ -19,6 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 //=======================================================================
+#pragma GCC diagnostic ignored "-Weverything"
 
 #include "AudioFile.h"
 #include <fstream>
